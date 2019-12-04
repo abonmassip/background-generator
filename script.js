fbfb7c77c67e85d47d46eea297d66556bc13c63a
Albert <abonmassip@gmail.com>
2019-12-04 10:23:37 -0800

    corrected linear-gradient line

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ function resetColors() {
     color1.value = "#ff0000"
     color2.value = "#ffff00"
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-    css.textContent = body.style.background + ";";
+    // css.textContent = body.style.background + ";";
+    css.textContent = `linear-gradient(to right, ${hexRgb(color1.value)}, ${hexRgb(color2.value)});`;
 }
 
 
 function setGradient() {
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-    css.textContent = body.style.background + ";";
+    // css.textContent = body.style.background + ";";
+    css.textContent = `linear-gradient(to right, ${hexRgb(color1.value)}, ${hexRgb(color2.value)});`;
 }
 
 var shuffle = function (array) {
@@ -44,8 +46,34 @@ function setRandomColors() {
     color1.value = generateColor();
     color2.value = generateColor();
     body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-    css.textContent = body.style.background + ";";
+    // css.textContent = body.style.background + ";";
+    css.textContent = `linear-gradient(to right, ${hexRgb(color1.value)}, ${hexRgb(color2.value)});`;
 }
+
+const hexa = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'];
+
+const hexDec = (par) => { // convert number. HEX to DEC
+    let parArray = par.split("");
+    let decArray = [];
+    parArray.forEach(n => {
+        hexa.forEach((value, index) => {
+            if (value.toString() === n.toString()) {
+                decArray.push(index);
+            }
+        });
+    })
+    return (decArray[0] * 16) + decArray[1];
+}
+
+const hexRgb = (par) => { // convert color code. HEX to RGB
+    let hexArray = par.split("");
+    hexArray.shift();
+    const num1 = hexDec(hexArray[0].toUpperCase().concat(hexArray[1]).toUpperCase());
+    const num2 = hexDec(hexArray[2].toUpperCase().concat(hexArray[3]).toUpperCase());
+    const num3 = hexDec(hexArray[4].toUpperCase().concat(hexArray[5]).toUpperCase());
+    return (`rgb(${num1}, ${num2}, ${num3})`);
+}
+
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
